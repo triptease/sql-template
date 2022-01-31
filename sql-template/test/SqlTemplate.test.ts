76a4c80dde889a1917148c908f749b457d26df0d
Max Pimm <maxmil@gmail.com>
2022-01-31 16:22:36 +0100

    Assert on full SqlTemplate object rather than having multiple assertions on it's properties

--- sql-template/test/SqlTemplate.test.ts
+++ sql-template/test/SqlTemplate.test.ts
@@ -1,61 +1,66 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {raw, SQL, SqlTemplate, spread} from '@triptease/sql-template';
+import {raw, SQL, SqlTemplate, spread, sqlTemplate} from '@triptease/sql-template';
 
 describe('SQL', () => {
     it('works with an empty expression', function() {
-        const fragment: SqlTemplate = SQL``;
-        expect(fragment.text).to.eql('');
-        expect(fragment.values).to.eql([]);
+        expect(SQL``).to.eql(sqlTemplate());
     });
 
     it('works with no text and multiple values', function() {
-        const fragment: SqlTemplate = SQL`${'a'}${'b'}`;
-        expect(fragment.text).to.eql('??');
-        expect(fragment.values).to.eql(['a', 'b']);
+        expect(SQL`${'a'}${'b'}`).to.eql(sqlTemplate(
+            '??',
+            ['a', 'b']
+        ));
     });
 
     it('does not introduce any spaces', function() {
-        const fragment: SqlTemplate = SQL`Hello${1}Sql${2}`;
-        expect(fragment.text).to.eql('Hello?Sql?');
-        expect(fragment.values).to.eql([1, 2]);
+        expect(SQL`Hello${1}Sql${2}`).to.eql(sqlTemplate(
+            'Hello?Sql?',
+            [1, 2]
+        ));
     });
 
     it('handles null', function() {
-        const fragment: SqlTemplate = SQL`SELECT * FROM users WHERE name = ${null}`;
-        expect(fragment.text).to.eql('SELECT * FROM users WHERE name = ?');
-        expect(fragment.values).to.eql([null]);
+        expect(SQL`SELECT * FROM users WHERE name = ${null}`).to.eql(sqlTemplate(
+            'SELECT * FROM users WHERE name = ?',
+            [null]
+        ));
     });
 
     it('maps undefined to null', function() {
-        const fragment: SqlTemplate = SQL`SELECT * FROM users WHERE name = ${undefined}`;
-        expect(fragment.text).to.eql('SELECT * FROM users WHERE name = ?');
-        expect(fragment.values).to.eql([null]);
+        expect(SQL`SELECT * FROM users WHERE name = ${undefined}`).to.eql(sqlTemplate(
+            'SELECT * FROM users WHERE name = ?',
+            [null]
+        ));
     });
 
     it('creates a SqlFragment from a template literal', function() {
-        const fragment: SqlTemplate = SQL`SELECT * FROM users WHERE name = ${'dan'}`;
-        expect(fragment.text).to.eql('SELECT * FROM users WHERE name = ?');
-        expect(fragment.values).to.eql(['dan']);
+        expect(SQL`SELECT * FROM users WHERE name = ${'dan'}`).to.eql(sqlTemplate(
+            'SELECT * FROM users WHERE name = ?',
+            ['dan']
+        ));
     });
 
     it('can use a raw dynamic variable', function() {
         const dynamic = 'users';
-        const fragment: SqlTemplate = SQL`SELECT * FROM ${raw(dynamic)} WHERE name = ${'dan'}`;
-        expect(fragment.text).to.eql('SELECT * FROM users WHERE name = ?');
-        expect(fragment.values).to.eql(['dan']);
+        expect(SQL`SELECT * FROM ${raw(dynamic)} WHERE name = ${'dan'}`).to.eql(sqlTemplate(
+            'SELECT * FROM users WHERE name = ?',
+            ['dan']));
     });
 
     it('can keep a array as a single value', function() {
-        const fragment: SqlTemplate = SQL`${['Dan', 'Bodart']}`;
-        expect(fragment.text).to.eql(`?`);
-        expect(fragment.values).to.eql([['Dan', 'Bodart']]);
+        expect(SQL`${['Dan', 'Bodart']}`).to.eql(sqlTemplate(
+            '?',
+            [['Dan', 'Bodart']]
+        ));
     });
 
     it('can spread an array into multiple values', function() {
-        const fragment: SqlTemplate = SQL`(${spread(['Dan', 'Bodart'])})`;
-        expect(fragment.text).to.eql(`(?, ?)`);
-        expect(fragment.values).to.eql(['Dan', 'Bodart']);
+        expect(SQL`(${spread(['Dan', 'Bodart'])})`).to.eql(sqlTemplate(
+            `(?, ?)`,
+            ['Dan', 'Bodart']
+        ));
     });
 
 });
